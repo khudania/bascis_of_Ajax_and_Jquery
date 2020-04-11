cd7a83cfb3ff97583eb2b610ba7b8de50e9a159e
Himanshu Khudania <hkhudania@gmail.com>
2020-04-11 15:20:01 +0530

    this keyword

--- main.js
+++ main.js
@@ -9,5 +9,14 @@ $(document).ready(() => {
     // $("div").click(() => console.log("Div is clicked"))
 
     // Selecting all elements on Page
-    $("*").click(() => console.log("All Elements Selected"))
-})
+    // $("*").click(() => console.log("All Elements Selected"))
+
+    // this keyword Selector(with arrow function hide is not working)
+    $("div").click(function () {
+        $("div").click(function () {
+            console.log("Div is clicked");
+            console.log($(this));
+            $(this).hide();
+        });
+    });
+});
